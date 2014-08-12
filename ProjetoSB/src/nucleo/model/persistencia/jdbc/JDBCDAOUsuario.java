fbad0444e39471aa88853a1ea1f3d214a55c7bcc
vbwarley <vitalwarley@gmail.com>
2014-08-11 23:02:14 -0300

    Criação da classe JDBCDAOUsuario, mas não totalmente implementada.

---
+++ ProjetoSB/src/nucleo/model/persistencia/jdbc/JDBCDAOUsuario.java
@@ -0,0 +1,39 @@
+package nucleo.model.persistencia.jdbc;
+
+import java.util.List;
+
+import nucleo.model.persistencia.dao.DAOUsuario;
+
+public class JDBCDAOUsuario<Usuario, String> implements DAOUsuario<Usuario, String> {
+
+	@Override
+	public void criar(Usuario objeto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Usuario consultar(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void alterar(Usuario objeto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(Usuario objeto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Usuario> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
