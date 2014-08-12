e11db86ab1268f8519c0804db611b6cda82d89d0
vbwarley <vitalwarley@gmail.com>
2014-08-11 22:05:09 -0300

    Criação do pacote jdbc, mas apenas com a classe JDBCDAO. Essa classe contém os métodos que serão herdados pelos demais DAOs.

---
+++ ProjetoSB/src/nucleo/model/persistencia/jdbc/JDBCDAO.java
@@ -0,0 +1,32 @@
+package nucleo.model.persistencia.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class JDBCDAO {
+
+	private Connection connection;
+	
+	private JDBCDAO() {
+		
+	}
+	
+	public void abrirConexao() {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:/banco_sb","","");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void fecharConexao() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
