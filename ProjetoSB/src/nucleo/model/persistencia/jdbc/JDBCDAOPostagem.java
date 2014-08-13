4ab36d4aa6af7cae4c16709aa3d6907a8c96da30
nathaliav <vital_nathalia@hotmail.com>
2014-08-13 18:01:48 -0300

    Criação do método consultar da classe JDBCDAOPostagem. Alterações no método criar da classe JDBCDAOPostagem.

--- ProjetoSB/src/nucleo/model/persistencia/jdbc/JDBCDAOPostagem.java
+++ ProjetoSB/src/nucleo/model/persistencia/jdbc/JDBCDAOPostagem.java
@@ -1,59 +1,86 @@
 package nucleo.model.persistencia.jdbc;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import nucleo.model.negocios.Postagem;
 import nucleo.model.persistencia.dao.DAOPostagem;
 
-public class JDBCDAOPostagem extends JDBCDAO implements DAOPostagem<Postagem, Integer>{
+public class JDBCDAOPostagem extends JDBCDAO implements
+		DAOPostagem<Postagem, Integer> {
 
 	@Override
 	public void criar(Postagem objeto) {
 		String sql = "INSERT INTO postagem VALUES (?,?,?,?,?)";
-		String sqlPP = "INSERT INTO postagem_palavras (?,?)";
-		String sqlP = "INSERT INTO palavras_chaves (?,?)";
-		
+
 		try {
-			
+
 			PreparedStatement stmt = getConnection().prepareStatement(sql);
-			PreparedStatement stmtPalavras_chave = getConnection().prepareStatement(sqlP);
-			PreparedStatement stmtPostagem_palavras = getConnection().prepareStatement(sqlPP);
-			
+
 			stmt.setInt(1, objeto.getCodigo());
 			stmt.setString(2, objeto.getTitulo());
 			stmt.setString(3, objeto.getConteudo());
-			
+			stmt.setInt(4, objeto.getBlog().getCodigo());
+
 			stmt.execute();
-					
-			
-		} catch (Exception e) {
-			
+
+		} catch (SQLException e) {
+			throw new RuntimeException();
+
 		}
 
 	}
 
 	@Override
 	public Postagem consultar(Integer id) {
-		
-		return null;
+		String PostagemSQL = "Select * from postagem where codigo = ?";
+		Postagem p = null;
+
+		JDBCDAOBlog b = new JDBCDAOBlog();
+
+		try {
+			abrirConexao();
+
+			PreparedStatement stmt = getConnection().prepareStatement(
+					PostagemSQL);
+
+			stmt.setInt(1, id);
+
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				p = new Postagem();
+
+				p.setCodigo(rs.getInt(1));
+				p.setTitulo(rs.getString(2));
+				p.setConteudo(rs.getString(3));
+				p.setBlog(b.consultar(rs.getInt(4)));
+
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException();
+
+		}
+
+		return p;
 	}
 
 	@Override
 	public void alterar(Postagem objeto) {
-		
-		
+
 	}
 
 	@Override
 	public void deletar(Postagem objeto) {
-		
-		
+
 	}
 
 	@Override
 	public List<Postagem> getList() {
-		
+
 		return null;
 	}
 }
