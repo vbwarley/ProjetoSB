124bb8baa116cac818a45d00d817a7a834f7b0d4
vbwarley <vitalwarley@gmail.com>
2014-08-17 16:27:56 -0300

    Implementação completa.

--- ProjetoSB/src/nucleo/model/persistencia/jdbc/JDBCDAOPalavraChave.java
+++ ProjetoSB/src/nucleo/model/persistencia/jdbc/JDBCDAOPalavraChave.java
@@ -1,39 +1,140 @@
 package nucleo.model.persistencia.jdbc;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import nucleo.model.negocios.Blog;
+import nucleo.model.negocios.PalavraChave;
+import nucleo.model.negocios.Usuario;
 import nucleo.model.persistencia.dao.DAOPalavraChave;
 
-public class JDBCDAOPalavraChave extends JDBCDAO implements DAOPalavraChave {
+public class JDBCDAOPalavraChave extends JDBCDAO implements DAOPalavraChave<PalavraChave, Integer> {
 
 	@Override
-	public void criar(Object objeto) {
-		// TODO Auto-generated method stub
+	public void criar(PalavraChave objeto) {
+		abrirConexao();
+		String insertSql = "INSERT INTO palavras_chave VALUES (?,?)";
+
+		try {
+			PreparedStatement stmt = getConnection().prepareStatement(insertSql);
+
+			stmt.setInt(1, objeto.getCodigo());
+			stmt.setString(2, objeto.getNome());
+			
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		} finally {
+			fecharConexao();
+		}		
+	}
+
+	@Override
+	public PalavraChave consultar(Integer id) {
+		abrirConexao();
+		String selectSql = "SELECT * FROM palavras_chave WHERE codigo = ?";
 		
-	}
-
-	@Override
-	public Object consultar(Object id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void alterar(Object objeto) {
-		// TODO Auto-generated method stub
+		PalavraChave pc = null;
 		
-	}
-
-	@Override
-	public void deletar(Object objeto) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement stmt = getConnection().prepareStatement(selectSql);
+			stmt.setInt(1, id);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				pc = new PalavraChave();
+				
+				pc.setCodigo(id);
+				pc.setNome(rs.getString(2));
+			}
+			
+			stmt.close();
+			rs.close();
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		} finally {
+			fecharConexao();
+		}
 		
+		return pc;
 	}
 
 	@Override
-	public List getList() {
-		// TODO Auto-generated method stub
-		return null;
+	public void alterar(PalavraChave objeto) {
+		abrirConexao();
+		String updateSql = "UPDATE palavras_chave SET nome=? WHERE codigo = ?";
+		
+		
+		try {
+			PreparedStatement stmt = getConnection().prepareStatement(updateSql);
+		
+			stmt.setString(1, objeto.getNome());
+			stmt.setInt(2, objeto.getCodigo());
+			
+			stmt.executeUpdate();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		} finally {
+			fecharConexao();
+		}
 	}
 
+	@Override
+	public void deletar(PalavraChave objeto) {
+		abrirConexao();
+		String deleteSql = "DELETE FROM palavras_chave WHERE codigo=?";
+
+		try {
+			PreparedStatement stmt = getConnection()
+					.prepareStatement(deleteSql);
+
+			stmt.setInt(1, objeto.getCodigo());
+
+			stmt.executeUpdate();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		} finally {
+			fecharConexao();
+		}
+	}
+
+	@Override
+	public List<PalavraChave> getList() {
+		abrirConexao();
+		String sqlList = "SELECT * FROM palavras_chave";
+		List<PalavraChave> lpc = null;
+		PalavraChave pc = null;
+
+		try {
+			PreparedStatement stmt = getConnection().prepareStatement(sqlList);
+
+			ResultSet rs = stmt.executeQuery(sqlList);
+
+			while (rs.next()) {
+				pc = new PalavraChave();
+				lpc = new ArrayList<PalavraChave>();
+
+				pc.setCodigo(rs.getInt(1));
+				pc.setNome(rs.getString(2));
+
+				lpc.add(pc);
+			}
+			
+			stmt.close();
+			rs.close();
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		} finally {
+			fecharConexao();
+		}
+
+		return lpc;
+	}
 }
