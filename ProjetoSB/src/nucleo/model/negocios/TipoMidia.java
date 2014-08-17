b080c17a8123219e321d786d384ca02a0732afa5
vbwarley <vitalwarley@gmail.com>
2014-08-17 15:22:52 -0300

    Adição de IDs e métodos necessários.

--- ProjetoSB/src/nucleo/model/negocios/TipoMidia.java
+++ ProjetoSB/src/nucleo/model/negocios/TipoMidia.java
@@ -1,5 +1,23 @@
 package nucleo.model.negocios;
 
 public enum TipoMidia {
-	AUDIO, VIDEO, IMAGEM
+	AUDIO(1), VIDEO(2), IMAGEM(3);
+	
+	private int id;
+	
+	private TipoMidia(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public static TipoMidia porId(int id) {
+		for (TipoMidia m : values()) {
+			if (m.getId() == id)
+				return m;
+		}
+		return null;
+	}
 }
