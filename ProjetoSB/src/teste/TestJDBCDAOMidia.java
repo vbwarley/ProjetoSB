ba94369394e5589f4bfb12aa5471d635b154ec69
vbwarley <vitalwarley@gmail.com>
2014-08-21 22:40:33 -0300

    Implementações.

--- ProjetoSB/src/teste/TestJDBCDAOMidia.java
+++ ProjetoSB/src/teste/TestJDBCDAOMidia.java
@@ -1,5 +1,46 @@
 package teste;
 
+import static org.junit.Assert.assertEquals;
+import nucleo.model.negocios.Midia;
+import nucleo.model.negocios.TipoMidia;
+import nucleo.model.persistencia.jdbc.JDBCDAOMidia;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestJDBCDAOMidia {
 
+	private JDBCDAOMidia jdm;
+
+	@Before
+	public void before() {
+		jdm = new JDBCDAOMidia();
+	}
+
+	@Test
+	public void criar() {
+		Midia midia = new Midia();
+
+		midia.setTipo(TipoMidia.AUDIO);
+		midia.setNomeArquivo("Arquivo X");
+		// comentario e postagem
+
+		jdm.criar(midia);
+
+		assertEquals(true, jdm.consultar(midia.getCodigo()).equals(midia));
+	}
+
+	@Test
+	public void consultar() {
+		Midia midia = new Midia();
+
+		midia.setTipo(TipoMidia.AUDIO);
+		midia.setNomeArquivo("Arquivo X");
+		// comentario e postagem
+
+		jdm.criar(midia);
+
+		assertEquals(true, jdm.consultar(midia.getCodigo()).equals(midia));
+
+	}
 }
