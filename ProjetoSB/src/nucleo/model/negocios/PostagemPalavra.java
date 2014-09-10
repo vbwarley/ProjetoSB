7024be73177a35a2f532782680ccf507c992dc6c
nathaliav <vital_nathalia@hotmail.com>
2014-09-09 23:04:25 -0300

    Javadoc Inicial

--- ProjetoSB/src/nucleo/model/negocios/PostagemPalavra.java
+++ ProjetoSB/src/nucleo/model/negocios/PostagemPalavra.java
@@ -5,22 +5,46 @@ public class PostagemPalavra {
 	private Postagem postagem;
 	private PalavraChave palavraChave;
 	
+	/**
+	 * Método construtor de PostagemPalavra.
+	 * @author nathalia
+	 */
 	public PostagemPalavra() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
+	/**
+	 * Método que recupera uma postagem.
+	 * @return Postagem postagem
+	 * @author nathalia
+	 */
 	public Postagem getPostagem() {
 		return postagem;
 	}
 
+	/**
+	 * Método que atribui dados a uma postagem
+	 * @param postagem Postagem
+	 * @author nathalia
+	 */
 	public void setPostagem(Postagem postagem) {
 		this.postagem = postagem;
 	}
 
+	/**
+	 * Método que recupera uma palavra chave.
+	 * @return PalavraChave palavraChave
+	 * @author nathalia
+	 */
 	public PalavraChave getPalavraChave() {
 		return palavraChave;
 	}
 
+	/**
+	 * Método que atribui/cria uma palavra chave
+	 * @param palavraChave PalavraChave
+	 * @author nathalia
+	 */
 	public void setPalavraChave(PalavraChave palavraChave) {
 		this.palavraChave = palavraChave;
 	}
