ab2f181ed385f5cc0c855b8aba35f06d0c718ee2
nathaliav <vital_nathalia@hotmail.com>
2014-09-09 23:00:00 -0300

    Javadoc inicial

--- ProjetoSB/src/nucleo/model/negocios/Postagem.java
+++ ProjetoSB/src/nucleo/model/negocios/Postagem.java
@@ -2,6 +2,7 @@ package nucleo.model.negocios;
 
 import java.util.Observable;
 
+
 public class Postagem extends Observable {
 
 	private int codigo;
@@ -9,43 +10,93 @@ public class Postagem extends Observable {
 	private String conteudo;
 	private Blog blog;
 
+	/**
+	 * Método construtor de Postagem
+	 * @author nathalia
+	 */
 	public Postagem() {
 		// aqui sera chamado o update dos observadores
 	}
 
+	/**
+	 * Método que recupera o código de uma postagem.
+	 * @return int codigo - retorna o código de uma postagem.
+	 * @author nathalia
+	 */
 	public int getCodigo() {
 		return codigo;
 	}
 
+	/**
+	 * Método que atribui um código a uma postagem.
+	 * @param codigo int 
+	 * @author nathalia
+	 */
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
 
+	/**
+	 * Método que recupera o título de uma postagem.
+	 * @return String titulo 
+	 * @author nathalia
+	 */
 	public String getTitulo() {
 		return titulo;
 	}
 
+	/**
+	 * Método que atribui um título a uma postagem.
+	 * @param titulo String 
+	 * @author nathalia
+	 */
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
 
+	/**
+	 * Método que recupera o conteúdo de uma postagem.
+	 * @return String conteudo
+	 * @author nathalia
+	 */
 	public String getConteudo() {
 		return conteudo;
 	}
 
+	/**
+	 * Método que atribui dados ao conteúdo de uma postagem.
+	 * @param conteudo String
+	 * @author nathalia
+	 */
 	public void setConteudo(String conteudo) {
 		this.conteudo = conteudo;
 		// aqui sera chamado o update dos observadores
 	}
 
+	/**
+	 * Método que recupera um blog. (Chave estrangeira na classe Postagem)
+	 * @return Blog blog
+	 * @author nathalia
+	 */
 	public Blog getBlog() {
 		return blog;
 	}
 
+	/**
+	 * Método que atribui um código a um blog
+	 * @param blog Blog
+	 * @author nathalia
+	 */
 	public void setBlog(Blog blog) {
 		this.blog = blog;
 	}
 
+	/**
+	 * Método que verifica se a postagem que foi passada como parâmetro é igual a esta postagem.
+	 * @param postagem Postagem
+	 * @return boolean equals - retorna uma condição verdadeira se o código da postagem for igual ao código da classe Postagem. Senão, uma condição falsa.
+	 * @author nathalia
+	 */
 	public boolean equals(Postagem postagem) {
 		
 		if (postagem.getCodigo() == (this.codigo) 
