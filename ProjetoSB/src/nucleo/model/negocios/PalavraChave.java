0a8890944cfbfa8fb0e3078daaf2366c543ca656
nathaliav <vital_nathalia@hotmail.com>
2014-09-09 23:11:02 -0300

    Criação do javadoc da classe PalavraChave.

--- ProjetoSB/src/nucleo/model/negocios/PalavraChave.java
+++ ProjetoSB/src/nucleo/model/negocios/PalavraChave.java
@@ -6,25 +6,56 @@ public class PalavraChave {
 	private int codigo;
 	private String nome;
 	
+	
+	/**
+	 * Método construtor de palavra chave
+	 * @author nathalia 
+	 */
 	public PalavraChave() {
 	}
 
+	/**
+	 * Método que recupera o código de uma palavra chave.
+	 * @return Integer codigo - retorna o código de uma palavra chave
+	 * @author nathalia
+	 */
 	public Integer getCodigo() {
 		return codigo;
 	}
 
+	/**
+	 * Método que atribui um código a uma palavra chave.
+	 * @param codigo Integer
+	 * @author nathalia
+	 */
 	public void setCodigo(Integer codigo) {
 		this.codigo = codigo;
 	}
 
+	/**
+	 * Método que recupera o nome de uma palavra chave.
+	 * @return String nome - retorna o nome de uma palavra chave
+	 * @author nathalia
+	 */
 	public String getNome() {
 		return nome;
 	}
 
+	/**
+	 * Método que atribui um nome a uma palavra chave.
+	 * @param nome String
+	 * @author nathalia
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 	
+	/**
+	 * Método que verifica se a palavra chave que foi passada como parâmetro é igual a esta palavra chave.
+	 * @param palavraChave PalavraChave
+	 * @return boolean equals - retorna uma condição verdadeira ou falsa.
+	 * @author nathalia
+	 */
 	public boolean equals(PalavraChave palavraChave) {
 		if (palavraChave.getCodigo() == this.codigo
 				&& palavraChave.getNome().equals(this.nome)) 
