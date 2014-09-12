b29f3e290eb2a5f1ebe078152807d10640f143ab
vbwarley <vitalwarley@gmail.com>
2014-09-12 18:02:22 -0300

    Javadoc da Assinatura completo.

--- ProjetoSB/src/nucleo/model/negocios/Assinatura.java
+++ ProjetoSB/src/nucleo/model/negocios/Assinatura.java
@@ -1,29 +1,60 @@
 package nucleo.model.negocios;
 
+/**
+ * Esta classe representa a relação de assinatura entre um usuário e um blog.
+ * 
+ * @see {@link Usuario}
+ * @see {@link Blog}
+ * @author Warley Vital
+ */
 public class Assinatura {
-	
+
 	private Usuario usuario;
 	private Blog blog;
-	
+
+	/**
+	 * Construtor da classe.
+	 */
 	public Assinatura() {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Retorna o usuário associado à assinatura.
+	 * 
+	 * @return uma instância de Usuario
+	 */
 	public Usuario getUsuario() {
 		return usuario;
 	}
 
+	/**
+	 * Seta o usuário referente à assinatura.
+	 * 
+	 * @param uma
+	 *            instância de Usuario
+	 */
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
 
+	/**
+	 * Retorna o blog associado à assinatura.
+	 * 
+	 * @return uma instância de Blog
+	 */
 	public Blog getBlog() {
 		return blog;
 	}
 
+	/**
+	 * Seta o blog referente à assinatura.
+	 * 
+	 * @param uma
+	 *            instância de Blog
+	 */
 	public void setBlog(Blog blog) {
 		this.blog = blog;
 	}
-	
-	
+
 }
