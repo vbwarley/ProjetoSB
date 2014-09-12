789402c41a1cc4d29577098847bd0cd503c3258c
nathaliav <vital_nathalia@hotmail.com>
2014-09-12 13:11:04 -0300

    Javadoc, classe mídia.

--- ProjetoSB/src/nucleo/model/negocios/Midia.java
+++ ProjetoSB/src/nucleo/model/negocios/Midia.java
@@ -8,50 +8,109 @@ public class Midia {
 	private ComentarioComposite comentario;
 	private Postagem postagem;
 
+	/**
+	 * Método construtor de Mídia
+	 * @author nathalia
+	 */
 	public Midia() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
+	/**
+	 * Método que recupera o código de uma mídia
+	 * @return int codigo
+	 * @author nathalia
+	 */
 	public int getCodigo() {
 		return codigo;
 	}
 
+	/**
+	 * Método que atribui um código a uma mídia
+	 * @param codigo int
+	 * @author nathalia
+	 */
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
 
+	/**
+	 * Método que recupera o tipo de uma mídia
+	 * @return TipoMidia tipo - retorna o tipo da postagem
+	 * @author nathalia
+	 */
 	public TipoMidia getTipo() {
 		return tipo;
 	}
 
+	/**
+	 * Método que atribui um tipo a uma mídia
+	 * @param tipo TipoMidia
+	 * @author nathalia
+	 */
 	public void setTipo(TipoMidia tipo) {
 		this.tipo = tipo;
 	}
 
+	/**
+	 * Método que recupera o nome do arquivo de uma mídia
+	 * @return String nomeArquivo
+	 * @author nathalia
+	 */
 	public String getNomeArquivo() {
 		return nomeArquivo;
 	}
 
+	/**
+	 * Método que atribui um nome em uma mídia
+	 * @param nomeArquivo String
+	 * @author nathalia
+	 */
 	public void setNomeArquivo(String nomeArquivo) {
 		this.nomeArquivo = nomeArquivo;
 	}
 
+	/**
+	 * Método que recupera um comentário
+	 * @return ComentarioComposite comentario
+	 * @author nathalia
+	 */
 	public ComentarioComposite getComentario() {
 		return comentario;
 	}
 
+	/**
+	 * Método que atribui um comentário a uma mídia
+	 * @param comentario ComentarioComposite
+	 * @author nathalia
+	 */
 	public void setComentario(ComentarioComposite comentario) {
 		this.comentario = comentario;
 	}
 
+	/**
+	 * Método que recupera uma postagem
+	 * @return Postagem postagem
+	 * @author nathalia
+	 */
 	public Postagem getPostagem() {
 		return postagem;
 	}
 
+	/**
+	 * Método que atribui uma postagem
+	 * @param postagem Postagem
+	 * @author nathalia
+	 */
 	public void setPostagem(Postagem postagem) {
 		this.postagem = postagem;
 	}
 
+	/**
+	 * Método que verifica se a midia que foi passada como parâmetro é igual a esta midia.
+	 * @param midia Midia
+	 * @return boolean true (se a verificação for verdadeira), false (se a verificação for falsa)
+	 */
 	public boolean equals(Midia midia) {
 
 		if (midia.getCodigo() == this.codigo
