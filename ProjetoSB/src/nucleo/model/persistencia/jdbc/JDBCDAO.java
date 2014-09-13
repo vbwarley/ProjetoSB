4e8c0795e4e1393257465dac2ff1711bbe7b1520
vbwarley <vitalwarley@gmail.com>
2014-09-12 22:38:47 -0300

    JavaDoc completo em JDBCDAO.

--- ProjetoSB/src/nucleo/model/persistencia/jdbc/JDBCDAO.java
+++ ProjetoSB/src/nucleo/model/persistencia/jdbc/JDBCDAO.java
@@ -4,41 +4,60 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Classe abstrata responsável pela conexão com o banco.
+ * 
+ * @author Warley Vital
+ */
 public abstract class JDBCDAO {
 
 	private Connection connection;
-	
+
+	/**
+	 * Construtor da classe.
+	 */
 	public JDBCDAO() {
-		
+
 	}
-	
+
+	/**
+	 * Este método abre a conexão com banco.
+	 * 
+	 */
 	protected void abrirConexao() {
 		try {
 			if (connection == null)
-				connection = DriverManager.getConnection("jdbc:mysql://localhost/SuperBlogs","root","mynewpassword");
-			else
-				if (connection.isClosed()) {
-					connection = DriverManager.getConnection("jdbc:mysql://localhost/SuperBlogs","root","mynewpassword");
-				}
+				connection = DriverManager.getConnection(
+						"jdbc:mysql://localhost/SuperBlogs", "root",
+						"mynewpassword");
+			else if (connection.isClosed())
+				connection = DriverManager.getConnection(
+						"jdbc:mysql://localhost/SuperBlogs", "root",
+						"mynewpassword");
+
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Este método fecha a conexão com o banco.
+	 */
 	protected void fecharConexao() {
 		try {
-			if (!connection.isClosed()) {
+			if (!connection.isClosed()) 
 				connection.close();
-			} else {
-//				connection = DriverManager.getConnection("jdbc:mysql://localhost/SuperBlogs","root","mynewpassword");
-			}
+			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
+	/**
+	 * Este método retorna a conexão atual.
+	 * 
+	 * @return conexão atual, ou null se não houver
+	 */
 	protected Connection getConnection() {
 		return connection;
 	}
