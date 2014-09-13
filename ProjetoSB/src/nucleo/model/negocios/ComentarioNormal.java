a22062d685151891a51414690911d8a8517ae638
DouglasMaximo <douglas.henrique.ifal@gmail.com>
2014-09-13 21:52:20 +0000

    update ComentarioNormal.
    Comentários Javadoc.

--- ProjetoSB/src/nucleo/model/negocios/ComentarioNormal.java
+++ ProjetoSB/src/nucleo/model/negocios/ComentarioNormal.java
@@ -2,6 +2,10 @@ package nucleo.model.negocios;
 
 public class ComentarioNormal extends ComentarioComposite {
 	
+	/**
+	 * Construtor da classe ComentarioNormal
+	 * @author Douglas
+	 */
 	public ComentarioNormal() {
 		// TODO Auto-generated constructor stub
 	}
