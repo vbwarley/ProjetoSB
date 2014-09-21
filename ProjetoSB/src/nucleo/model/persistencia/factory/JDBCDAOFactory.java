0495f5ac89f6021ad65a5fa87325aac089ca505c
vbwarley <vitalwarley@gmail.com>
2014-09-21 20:38:40 -0300

    Modificações simples no javadoc.

--- ProjetoSB/src/nucleo/model/persistencia/factory/JDBCDAOFactory.java
+++ ProjetoSB/src/nucleo/model/persistencia/factory/JDBCDAOFactory.java
@@ -19,38 +19,60 @@ import nucleo.model.persistencia.jdbc.JDBCDAOPalavraChave;
 import nucleo.model.persistencia.jdbc.JDBCDAOPostagem;
 import nucleo.model.persistencia.jdbc.JDBCDAOUsuario;
 
+/**
+ * Fábrica de DAOs.
+ * @author Warley Vital
+ */
 public class JDBCDAOFactory extends DAOFactory {
 
+	/* (non-Javadoc)
+	 * @see nucleo.model.persistencia.factory.DAOFactory#getDAOUsuario()
+	 */
 	@Override
 	public DAOUsuario<Usuario, String> getDAOUsuario() {
 		DAOUsuario<Usuario, String> usuario = new JDBCDAOUsuario();
 		return usuario;
 	}
 
+	/* (non-Javadoc)
+	 * @see nucleo.model.persistencia.factory.DAOFactory#getDAOBlog()
+	 */
 	@Override
 	public DAOBlog<Blog, Integer> getDAOBlog() {
 		DAOBlog<Blog, Integer> blog = new JDBCDAOBlog();
 		return blog;
 	}
 
+	/* (non-Javadoc)
+	 * @see nucleo.model.persistencia.factory.DAOFactory#getDAOPostagem()
+	 */
 	@Override
 	public DAOPostagem<Postagem, Integer> getDAOPostagem() {
 		DAOPostagem<Postagem, Integer> postagem = new JDBCDAOPostagem();
 		return postagem;
 	}
 
+	/* (non-Javadoc)
+	 * @see nucleo.model.persistencia.factory.DAOFactory#getDAOMidia()
+	 */
 	@Override
 	public DAOMidia<Midia, Integer> getDAOMidia() {
 		DAOMidia<Midia, Integer> midia = new JDBCDAOMidia();
 		return midia;
 	}
 
+	/* (non-Javadoc)
+	 * @see nucleo.model.persistencia.factory.DAOFactory#getDAOComentario()
+	 */
 	@Override
 	public DAOComentario<ComentarioComposite, Integer> getDAOComentario() {
 		DAOComentario<ComentarioComposite, Integer> comentario = new JDBCDAOComentario();
 		return comentario;
 	}
 
+	/* (non-Javadoc)
+	 * @see nucleo.model.persistencia.factory.DAOFactory#getDAOPalavraChave()
+	 */
 	@Override
 	public DAOPalavraChave<PalavraChave, Integer> getDAOPalavraChave() {
 		DAOPalavraChave<PalavraChave, Integer> palavraChave = new JDBCDAOPalavraChave();
